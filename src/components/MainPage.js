2f5ae89acbee60a406507a3a89d90654a73975dc
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-09 01:48:15 +0330

    background added and scrollbar customized

---
+++ src/components/MainPage.js
@@ -0,0 +1,11 @@
+import {ReactComponent as Pattern} from "../assets/images/main-pattern.svg";
+
+const MainPage = () => {
+    return(
+        <div>
+            <Pattern/>
+        </div>
+    )
+}
+
+export default MainPage;

011cdb1ef88203e5f1a338f53458da40bee358ef
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-09 15:16:36 +0330

    added texts

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,17 @@
 import {ReactComponent as Pattern} from "../assets/images/main-pattern.svg";
 import IconTitle from "./IconTitle";
+import '../stylesheets/main-page.scss';
 
 const MainPage = () => {
     return(
         <div>
             <IconTitle/>
+            <p id={'orange-text'}>
+                نويسه خوان الفبا
+            </p>
+            <p id={'white-text'}>
+                برای تبدیل عکس به نوشته
+            </p>
             <Pattern/>
         </div>
     )

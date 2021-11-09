7ae14d2b707a5e41f1e95b0f5ffb94786008e63b
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-09 15:28:18 +0330

    the starting page is implemented

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,5 @@
 import {ReactComponent as Pattern} from "../assets/images/main-pattern.svg";
+import {ReactComponent as Papers} from "../assets/images/papers.svg";
 import IconTitle from "./IconTitle";
 import '../stylesheets/main-page.scss';
 
@@ -12,6 +13,9 @@ const MainPage = () => {
             <p id={'white-text'}>
                 برای تبدیل عکس به نوشته
             </p>
+            <div id={'papers'}>
+                <Papers id={'paper-image'}/>
+            </div>
             <Pattern/>
         </div>
     )

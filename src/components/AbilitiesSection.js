4606e5433c03ee331280d850d4f853a5b62fb9ad
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-11 08:33:47 +0330

    abilities group in web is developed and implemented

--- src/components/AbilitiesSection.js
+++ src/components/AbilitiesSection.js
@@ -1,5 +1,11 @@
 import '../stylesheets/abilities-section.scss';
 import {ReactComponent as Side} from "../assets/images/abilities/side.svg";
+import {ReactComponent as Right1} from "../assets/images/abilities/right-1.svg";
+import {ReactComponent as Right2} from "../assets/images/abilities/right-2.svg";
+import {ReactComponent as Right3} from "../assets/images/abilities/right-3.svg";
+import {ReactComponent as Left1} from "../assets/images/abilities/left-1.svg";
+import {ReactComponent as Left2} from "../assets/images/abilities/left-2.svg";
+import {ReactComponent as Left3} from "../assets/images/abilities/left-3.svg";
 
 const AbilitiesSection = () => {
     return(
@@ -11,56 +17,65 @@ const AbilitiesSection = () => {
                 <Side id={'side-main'}/>
             </div>
             <div style={{height: '5vh'}}/>
-            <div id={'right'}>
-                <div id={'right-1'}>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
+            <div id={'abilities-wrapper'}>
+                <div id={'left'}>
+                    <div style={{height: '15vh'}}/>
+                    <div className={'ability-group'} id={'left-1'}>
+                        <Left1/>
+                        <p className={'black-text'}>
+                            حواندن متون فارسی و انگلیسی
+                        </p>
+                        <p className={'white-text'}>
+                            خواندن نویسه های فارسی، انگلیسی و البته زبان های مشابه مثل عربی و دری،همه در کنار هم در یک تصویر
+                        </p>
+                    </div>
+                    <div className={'ability-group'} id={'left-2'}>
+                        <Left2/>
+                        <p className={'black-text'}>
+                            اصلاح تصویر
+                        </p>
+                        <p className={'white-text'}>
+                            اصلاح نویز، چرخض و سایه تصویر برای دقیق تر خواندن متن
+                        </p>
+                    </div>
+                    <div className={'ability-group'} id={'left-3'}>
+                        <Left3/>
+                        <p className={'black-text'}>
+                            خواندن حرکت های حروف
+                        </p>
+                        <p className={'white-text'}>
+                            خواندن نوشته ها و حذف حرکت ها از عبارت های فارسی یا عربی مثل آیات قرآن کریم
+                        </p>
+                    </div>
                 </div>
-                <div id={'right-2'}>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
-                </div>
-                <div id={'right-3'}>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
-                </div>
-            </div>
-            <div id={'left'}>
-                <div id={'left-1'}>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
-                </div>
-                <div id={'left-2'}>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
-                </div>
-                <div id={'left-3'}>
-                    <p>
-
-                    </p>
-                    <p>
-
-                    </p>
+                <div id={'right'}>
+                    <div className={'ability-group'} id={'right-1'}>
+                        <Right1/>
+                        <p className={'black-text'}>
+                            خواندن قلم های گوناگون
+                        </p>
+                        <p className={'white-text'}>
+                            خواندن متون نوشته شده با قلم های مختلف،مثل عنوان، متن یا پاورقی
+                        </p>
+                    </div>
+                    <div className={'ability-group'} id={'right-2'}>
+                        <Right2/>
+                        <p className={'black-text'}>
+                            حفظ ساختار سند
+                        </p>
+                        <p className={'white-text'}>
+                            چیدن عنوان، متن، تصویر و جدول در صفحه نتیجه، برای حفظ ساختار سند مورد تحلیل
+                        </p>
+                    </div>
+                    <div className={'ability-group'} id={'right-3'}>
+                        <Right3/>
+                        <p className={'black-text'}>
+                            اطمینان از درستی
+                        </p>
+                        <p className={'white-text'}>
+                            مشخص کردن بخش هایی از نوشته که با تردید خوانده شده اند برای بازنگری توسط انسان
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>

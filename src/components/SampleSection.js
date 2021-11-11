3f1d8036d45498b52d37e51893dd1b07055a73c1
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-11 16:21:08 +0330

    added title

--- src/components/SampleSection.js
+++ src/components/SampleSection.js
@@ -4,42 +4,48 @@ import '../stylesheets/sample-section.scss';
 
 const SampleSection = () => {
     return(
-        <div id={'sample-section'}>
-            <img src={Sample1} id={'image-sample'} alt={''}/>
-            <div style={{width: '20vw'}}/>
-            <div id={'buttons'}>
-                <div className={'button'}>
-                    <div className={'sample-row'}>
-                        <p className={'white-title-sample'}>
-                            حفظ ساختار سند
+        <div className={'sample'}>
+            <p id={'sample-title'}>
+                نمونه های الفبا
+            </p>
+            <div style={{height: '10vw'}}/>
+            <div id={'sample-section'}>
+                <img src={Sample1} id={'image-sample'} alt={''}/>
+                <div style={{width: '20vw'}}/>
+                <div id={'buttons'}>
+                    <div className={'button'}>
+                        <div className={'sample-row'}>
+                            <p className={'white-title-sample'}>
+                                حفظ ساختار سند
+                            </p>
+                            <Dot className={'dot'}/>
+                        </div>
+                        <p className={'description-sample'}>
+                            جدول درون سند، درست با همان قالب ردیف ها و ستون ها تحلیل میشود.
                         </p>
-                        <Dot className={'dot'}/>
                     </div>
-                    <p className={'description-sample'}>
-                        جدول درون سند، درست با همان قالب ردیف ها و ستون ها تحلیل میشود.
-                    </p>
-                </div>
-                <div className={'button'}>
-                    <div className={'sample-row'}>
-                        <p className={'white-title-sample'}>
-                            اصلاح تصویر
+                    <div className={'button'}>
+                        <div className={'sample-row'}>
+                            <p className={'white-title-sample'}>
+                                اصلاح تصویر
+                            </p>
+                            <Dot className={'dot'}/>
+                        </div>
+                        <p className={'description-sample'}>
+                            پس زمینه نوشته ها اگر تمیز هم نباشد، الفبا میتواند آن را بخواند.
                         </p>
-                        <Dot className={'dot'}/>
                     </div>
-                    <p className={'description-sample'}>
-                        پس زمینه نوشته ها اگر تمیز هم نباشد، الفبا میتواند آن را بخواند.
-                    </p>
-                </div>
-                <div className={'button'}>
-                    <div className={'sample-row'}>
-                        <p className={'white-title-sample'}>
-                            حفظ ساختار متن
+                    <div className={'button'}>
+                        <div className={'sample-row'}>
+                            <p className={'white-title-sample'}>
+                                حفظ ساختار متن
+                            </p>
+                            <Dot className={'dot'}/>
+                        </div>
+                        <p className={'description-sample'}>
+                            خطوط نوشته در قالب پاراگراف های متن به مخاطب ارائه میشود.
                         </p>
-                        <Dot className={'dot'}/>
                     </div>
-                    <p className={'description-sample'}>
-                        خطوط نوشته در قالب پاراگراف های متن به مخاطب ارائه میشود.
-                    </p>
                 </div>
             </div>
         </div>

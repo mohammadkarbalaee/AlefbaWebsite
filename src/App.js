55b2ee0f59eb5c775a113db89fcda684f7f3bd28
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-13 14:57:25 +0330

    footer is implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import UsagesSection from "./components/UsagesSection";
 import TechnicalUtils from "./components/TechnicalUtils";
 import SampleSection from "./components/SampleSection";
 import './App.scss';
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
       <TechnicalUtils/>
       <div style={{height: '25vw'}}/>
       <SampleSection/>
-      <div style={{height: '25vw'}}/>
+      <div style={{height: '20vw'}}/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import '../stylesheets/footer.scss';
+import {ReactComponent as Icon} from "../assets/images/main/logo.svg";
+
+const Footer = () => {
+    return(
+        <div id={'footer'}>
+            <div id={'email-footer'}>
+                <p id={'company'}>
+                    شرکت دانش بنیان ((راهکار پزدازش ژرف))
+                </p>
+                <a href={'https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=salam@roshan-ai.ir'} target={"_blank"} rel="noreferrer">
+                    <p id={'email'}>
+                        salam@roshan-ai.ir
+                    </p>
+                </a>
+            </div>
+            <div id={'contact-footer'}>
+                <a href={'https://www.roshan-ai.ir/contact-us/'} target={"_blank"} rel="noreferrer">
+                    <p id={'contact-text'}>
+                        تماس با ما
+                    </p>
+                </a>
+                <a href={'https://www.roshan-ai.ir/join-us/'} target={"_blank"} rel="noreferrer">
+                    <p id={'join-text'}>
+                        همکاری با ما
+                    </p>
+                </a>
+            </div>
+            <div id={'icon-title-footer'}>
+                <p id={'footer-title'}>
+                    الفبا
+                </p>
+                <div>
+                    <Icon id={'icon-footer'}/>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Footer;

261cf22ef036dcae28ab84b645cdd103965078a9
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-13 14:00:25 +0330

    restarting the implementation of the footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import UsagesSection from "./components/UsagesSection";
 import TechnicalUtils from "./components/TechnicalUtils";
 import SampleSection from "./components/SampleSection";
 import './App.scss';
-import Footer from "./components/Footer";
 
 function App() {
   return (

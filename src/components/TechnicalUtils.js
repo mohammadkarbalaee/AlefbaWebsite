d831cee2a8a68bd275c232412b0a7234efca2ed7
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-14 21:00:37 +0330

    something done

--- src/components/TechnicalUtils.js
+++ src/components/TechnicalUtils.js
@@ -17,7 +17,7 @@ const TechnicalUtils = () => {
             <div style={{height: '5vh'}}/>
             <div id={'utils-wrapper'}>
                 <div id={'left-utils'}>
-                    <div className={'util-group'} id={'left-1'}>
+                    <div className={'util-group right-to-left'} id={'left-1'}>
                         <div className={'util-row'}>
                             <p className={'black-util-text'}>
                                 دقت و سرعت
@@ -29,7 +29,22 @@ const TechnicalUtils = () => {
                             الفبا با دقت 97% نوشته‌های اسناد را می‌خواند و با سخت‌افزار مناسب میتواند 20 صفحه را در هر دقیقه تحلیل کند. البته دقت نویسه‌خوان به کیفیت اسناد ورودی هم ربط دارد.
                         </p>
                     </div>
-                    <div className={'util-group'} id={'left-2'}>
+                    <div className={'util-group left-to-right'} id={'right-1'}>
+                        <div className={'util-row left-to-right-row1'}>
+                            <p className={'black-util-text'}>
+                                پردازش زبان طبیعی
+                            </p>
+                            <div style={{width: '2vw'}}/>
+                            <Right1 className={'util-icon'}/>
+                        </div>
+                        <p className={'white-util-text left-to-right'}>
+                            الفبا جمله‌های فارسی را می‌شناسد. یعنی گاهی وقت‌ها که میان دو شکل مختلف یک عبارت تردید دارد،هر دو را در میان دیگر کلمات خط نوشته قرار می‌دهد، تا ببیند کدام یک از جملات را بهتر کامل می‌کند.
+                        </p>
+                    </div>
+                </div>
+                <div id={'right-utils'}>
+                    <div id={'right-space'}/>
+                    <div className={'util-group right-to-left'} id={'left-2'}>
                         <div className={'util-row'}>
                             <p className={'black-util-text'}>
                                 پردازش موازی
@@ -41,30 +56,15 @@ const TechnicalUtils = () => {
                             الفبا می‌تواند با استفاده از پردازنده گرافیکی(کارت گرافیک)،اسناد را چند برابر سریع‌تر پردازش کند. همچنین قابلیت استفاده از چند ماشین برای پردازش توزیع‌شده اسناد را فراهم می‌کند.
                         </p>
                     </div>
-                </div>
-                <div id={'right-utils'}>
-                    <div id={'right-space'}/>
-                    <div className={'util-group'} id={'right-1'}>
-                        <div className={'util-row'}>
-                            <p className={'black-util-text'}>
-                                پردازش زبان طبیعی
-                            </p>
-                            <div style={{width: '2vw'}}/>
-                            <Right1 className={'util-icon'}/>
-                        </div>
-                        <p className={'white-util-text'}>
-                            الفبا جمله‌های فارسی را می‌شناسد. یعنی گاهی وقت‌ها که میان دو شکل مختلف یک عبارت تردید دارد،هر دو را در میان دیگر کلمات خط نوشته قرار می‌دهد، تا ببیند کدام یک از جملات را بهتر کامل می‌کند.
-                        </p>
-                    </div>
-                    <div className={'util-group'} id={'right-2'}>
-                        <div className={'util-row'}>
+                    <div className={'util-group left-to-right'} id={'right-2'}>
+                        <div className={'util-row left-to-right-row2'}>
                             <p className={'black-util-text'}>
                                 یادگیری عمیق
                             </p>
                             <div style={{width: '2vw'}}/>
                             <Right2 className={'util-icon'}/>
                         </div>
-                        <p className={'white-util-text'}>
+                        <p className={'white-util-text left-to-right'}>
                             الفبا با کمک فناوری های روز دنیا یعنی یادگیری عمیق، خواندن نویسه‌ها را با قلم‌های مختلف یادگرفته است. این فناوری علاوه بر دقت بالا،امکان خواندن قلم‌های تازه را هم برای نویسه‌خوان فراهم می‌کند.
                         </p>
                     </div>

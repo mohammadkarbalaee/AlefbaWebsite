561d266f3138a96132202b229525487d01adc37b
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-14 15:22:33 +0330

    implemented usages sections mobile format

--- src/components/UsagesSection.js
+++ src/components/UsagesSection.js
@@ -5,42 +5,45 @@ import LibrariesImage from '../assets/images/usages/libraries.png';
 
 const UsagesSection = () => {
     return(
-        <div id={'usages-section'}>
+        <div>
             <p id={'usages-title'}>
                 کاربرد های الفبا
             </p>
-            <div style={{height: '8vw'}}/>
-            <div id={'in-businesses'}>
-                <img src={BusinessImage} id={'business-image'} alt={''}/>
-                <p className={'orange-text'}>
-                    در کسب‌و‌کار‌ها
-                </p>
-                <p className={'white-text'}>
-                    کاغذ و نوشته همه جا پیدا می‌شود
-                </p>
-            </div>
-            <div>
-                <div style={{height: '10vw'}}/>
-                <div id={'in-organizations'}>
-                    <img src={OrganizationsImage} id={'organization-image'} alt={''}/>
-                    <p className={'orange-text'}>
-                        در سازمان ها
+            <div id={'space-4'}/>
+            <div id={'usages-section'}>
+                <div id={'space-1'}/>
+                <div id={'in-businesses'}>
+                    <img src={BusinessImage} id={'business-image'} alt={''}/>
+                    <p className={'orange-text-usages'}>
+                        در کسب‌و‌کار‌ها
                     </p>
-                    <p className={'white-text'}>
-                        پرونده‌ها و نامه‌ها روی هم انباشته شده و در اتاق‌های بایگانی خاک می‌خورند. مسیر ((دولت الکترونیک)) از ورود این اسناد به دنیای دیجیتال میگذرد:
+                    <p className={'white-text-usages'}>
+                        کاغذ و نوشته همه جا پیدا می‌شود
                     </p>
                 </div>
-            </div>
-            <div>
-                <div style={{height: '30vw'}}/>
-                <div id={'in-libraries'}>
-                    <img src={LibrariesImage} id={'library-image'} alt={''}/>
-                    <p className={'orange-text'}>
-                        در کتابخانه
-                    </p>
-                    <p className={'white-text'}>
-                        صفحه‌های لا‌به‌لای قفسه‌های کتابخانه منتظر ورود به دنیای نو هستند.
-                    </p>
+                <div>
+                    <div id={'space-2'}/>
+                    <div id={'in-organizations'}>
+                        <img src={OrganizationsImage} id={'organization-image'} alt={''}/>
+                        <p className={'orange-text-usages'}>
+                            در سازمان ها
+                        </p>
+                        <p className={'white-text-usages'}>
+                            پرونده‌ها و نامه‌ها روی هم انباشته شده و در اتاق‌های بایگانی خاک می‌خورند. مسیر ((دولت الکترونیک)) از ورود این اسناد به دنیای دیجیتال میگذرد:
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <div id={'space-3'}/>
+                    <div id={'in-libraries'}>
+                        <img src={LibrariesImage} id={'library-image'} alt={''}/>
+                        <p className={'orange-text-usages'}>
+                            در کتابخانه
+                        </p>
+                        <p className={'white-text-usages'}>
+                            صفحه‌های لا‌به‌لای قفسه‌های کتابخانه منتظر ورود به دنیای نو هستند.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>

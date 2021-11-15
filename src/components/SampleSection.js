f2c1c531ef6c3adb6839f7d9613b2de4d1bb69c5
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-15 09:48:02 +0330

    sample mobile is done

--- src/components/SampleSection.js
+++ src/components/SampleSection.js
@@ -1,4 +1,7 @@
 import Sample1 from '../assets/images/samples/sample-1.png';
+import Sample1Mobile from '../assets/images/samples/sample-1-mobile.png';
+import Sample2Mobile from '../assets/images/samples/sample-2-mobile.png';
+import Sample3Mobile from '../assets/images/samples/sample-3-mobile.png';
 import {ReactComponent as Dot} from "../assets/images/samples/dot.svg";
 import '../stylesheets/sample-section.scss';
 
@@ -14,6 +17,7 @@ const SampleSection = () => {
                 <div style={{width: '20vw'}}/>
                 <div id={'buttons'}>
                     <div className={'button'}>
+                        <img src={Sample1Mobile} className={'mobile-image float-right'} id={'sample-1-mobile'} alt={''}/>
                         <div className={'sample-row'}>
                             <p className={'white-title-sample'}>
                                 حفظ ساختار سند
@@ -25,17 +29,19 @@ const SampleSection = () => {
                         </p>
                     </div>
                     <div className={'button'}>
+                        <img src={Sample2Mobile} className={'mobile-image float-left'} id={'sample-2-mobile'} alt={''}/>
                         <div className={'sample-row'}>
-                            <p className={'white-title-sample'}>
+                            <p className={'white-title-sample text-left-title'}>
                                 اصلاح تصویر
                             </p>
                             <Dot className={'dot'}/>
                         </div>
-                        <p className={'description-sample'}>
+                        <p className={'description-sample text-left'}>
                             پس‌زمینه نوشته اگر تمیز هم نباشد، الفبا می‌تواند آن را بخواند.
                         </p>
                     </div>
                     <div className={'button'}>
+                        <img src={Sample3Mobile} className={'mobile-image float-right'} id={'sample-3-mobile'} alt={''}/>
                         <div className={'sample-row'}>
                             <p className={'white-title-sample'}>
                                 حفظ ساختار متن

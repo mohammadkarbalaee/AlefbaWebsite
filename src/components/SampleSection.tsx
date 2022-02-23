1851d5a9aa3b1a4c02461ac12b3ec7f660a85ea2
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-02-23 10:43:38 +0330

    now using svg rather than pngs to implement animations more smoothly

--- src/components/SampleSection.tsx
+++ src/components/SampleSection.tsx
@@ -1,6 +1,6 @@
-import Sample1 from '../assets/images/samples/sample-1.png';
-import Sample2 from '../assets/images/samples/sample-2.png';
-import Sample3 from '../assets/images/samples/sample-3.png';
+import KartMelli from "./samples/KartMelli";
+import Sherr from "./samples/Sherr";
+import TarazNameh from "./samples/TarazNameh";
 import Border from '../assets/images/samples/border.png';
 import Sample1Mobile from '../assets/images/samples/sample-1-mobile.png';
 import Sample2Mobile from '../assets/images/samples/sample-2-mobile.png';
@@ -10,28 +10,33 @@ import {ReactComponent as Polygon} from "../assets/images/samples/polygon.svg";
 import '../stylesheets/sample-section.scss';
 import {useState} from "react";
 
+enum Sample {
+    KartMelli,
+    Sherr,
+    Taraznameh
+}
+
 const SampleSection = () => {
 
-    let [activeSample,setActiveSample] = useState(1);
+    let [activeSample,setActiveSample] = useState(Sample.KartMelli);
 
-    const onSample1Click = () => {
-        setActiveSample(1);
+    const onKartMelliClick = () => {
+        setActiveSample(Sample.KartMelli);
     }
 
-    const onSample2Click = () => {
-        setActiveSample(2);
+    const onTarazNamehClick = () => {
+        setActiveSample(Sample.Taraznameh);
     }
 
-    const onSample3Click = () => {
-        setActiveSample(3);
+    const onSherClick = () => {
+        setActiveSample(Sample.Sherr);
     }
 
     const whichSample = () => {
         switch (activeSample){
-            case 1: return Sample1;
-            case 2: return Sample2;
-            case 3: return Sample3;
-            default: return Sample1;
+            case Sample.KartMelli: return (<KartMelli/>);
+            case Sample.Sherr: return (<Sherr/>);
+            case Sample.Taraznameh: return (<TarazNameh/>);
         }
     }
 
@@ -42,48 +47,48 @@ const SampleSection = () => {
             </p>
             <div style={{height: '10vw'}}/>
             <div id={'sample-section'}>
-                <img src={whichSample()} id={'image-sample'} alt={''}/>
+                ({whichSample()})
                 <div  id={'orange-border'}>
                     <img src={Border} id={'orange-border-main'} alt={''}/>
                 </div>
                 <div style={{width: '20vw'}}/>
                 <div id={'buttons'}>
-                    <div className={'button'} onClick={onSample1Click}>
+                    <div className={'button'} onClick={onKartMelliClick}>
                         <img src={Sample1Mobile} className={'mobile-image float-right'} id={'sample-1-mobile'} alt={''}/>
                         <div className={'sample-row'}>
-                            <p className={'white-title-sample'} id={activeSample === 1 ? 'active-orange-title' : ''}>
+                            <p className={'white-title-sample'} id={activeSample === Sample.KartMelli ? 'active-orange-title' : ''}>
                                 حفظ ساختار سند
                             </p>
-                            <Dot className={activeSample === 1 ? 'no-dot' : 'dot'}/>
-                            <Polygon className={activeSample === 1 ? 'polygon' : 'no-dot'}/>
+                            <Dot className={activeSample === Sample.KartMelli ? 'no-dot' : 'dot'}/>
+                            <Polygon className={activeSample === Sample.KartMelli ? 'polygon' : 'no-dot'}/>
                         </div>
-                        <p className={'description-sample'} id={activeSample === 1 ? 'active-orange-description' : ''}>
+                        <p className={'description-sample'} id={activeSample === Sample.KartMelli ? 'active-orange-description' : ''}>
                             جدول درون سند، درست با همان قالب ردیف‌ها و ستون‌ها تحلیل می‌شود.
                         </p>
                     </div>
-                    <div className={'button'} onClick={onSample2Click}>
+                    <div className={'button'} onClick={onTarazNamehClick}>
                         <img src={Sample2Mobile} className={'mobile-image float-left'} id={'sample-2-mobile'} alt={''}/>
                         <div className={'sample-row'}>
-                            <p className={'white-title-sample text-left-title'} id={activeSample === 2 ? 'active-orange-title' : ''}>
+                            <p className={'white-title-sample text-left-title'} id={activeSample === Sample.Taraznameh ? 'active-orange-title' : ''}>
                                 اصلاح تصویر
                             </p>
-                            <Dot className={activeSample === 2 ? 'no-dot' : 'dot'}/>
-                            <Polygon className={activeSample === 2 ? 'polygon' : 'no-dot'}/>
+                            <Dot className={activeSample === Sample.Taraznameh ? 'no-dot' : 'dot'}/>
+                            <Polygon className={activeSample === Sample.Taraznameh ? 'polygon' : 'no-dot'}/>
                         </div>
-                        <p className={'description-sample text-left'} id={activeSample === 2 ? 'active-orange-description' : ''}>
+                        <p className={'description-sample text-left'} id={activeSample === Sample.Taraznameh ? 'active-orange-description' : ''}>
                             پس‌زمینه نوشته اگر تمیز هم نباشد، الفبا می‌تواند آن را بخواند.
                         </p>
                     </div>
-                    <div className={'button'} onClick={onSample3Click}>
+                    <div className={'button'} onClick={onSherClick}>
                         <img src={Sample3Mobile} className={'mobile-image float-right'} id={'sample-3-mobile'} alt={''}/>
                         <div className={'sample-row'}>
-                            <p className={'white-title-sample'} id={activeSample === 3 ? 'active-orange-title' : ''}>
+                            <p className={'white-title-sample'} id={activeSample === Sample.Sherr ? 'active-orange-title' : ''}>
                                 حفظ ساختار متن
                             </p>
-                            <Dot className={activeSample === 3 ? 'no-dot' : 'dot'}/>
-                            <Polygon className={activeSample === 3 ? 'polygon' : 'no-dot'}/>
+                            <Dot className={activeSample === Sample.Sherr ? 'no-dot' : 'dot'}/>
+                            <Polygon className={activeSample === Sample.Sherr ? 'polygon' : 'no-dot'}/>
                         </div>
-                        <p className={'description-sample'} id={activeSample === 3 ? 'active-orange-description' : ''}>
+                        <p className={'description-sample'} id={activeSample === Sample.Sherr ? 'active-orange-description' : ''}>
                             خطوط نوشته در قالب پاراگراف‌های متن به مخاطب ارائه می‌شوند.
                         </p>
                     </div>

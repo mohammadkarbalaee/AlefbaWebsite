440b0bc65c15ebf2941c9737f02e686a7ac703a5
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-02-23 11:58:06 +0330

    implemented the initial version of animations. but there are some responsive issues

---
+++ src/animations/typingAnimation.js
@@ -0,0 +1,23 @@
+export const TypingAnimation = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            duration: 3
+        }
+    },
+}
+
+export const ImageAnimation = {
+    hidden: {
+        opacity: 1,
+    },
+    visible: {
+        opacity: 0,
+        transition: {
+            duration: 3
+        }
+    },
+}
